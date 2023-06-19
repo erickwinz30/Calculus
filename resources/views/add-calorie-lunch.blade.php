da96553dde1cb3eae02e1c57dc5c857c35afff13
Erick Winata <erickwinz27@gmail.com>
2023-06-19 16:18:22 +0700

    Penambahan Tag Meta

--- resources/views/add-calorie-lunch.blade.php
+++ resources/views/add-calorie-lunch.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <meta name="dicoding:email" content="f124xb428@dicoding.org">
     <link rel="stylesheet" href="{{ asset('css/add-calorie.css') }}">
     <title>Calculus | Add Calorie</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
